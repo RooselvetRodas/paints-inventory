395c7e2c753da9e588b40d45191aad6da848072f
RooselvetRodas <rooselvet.bolanos@nttdata.com>
2025-09-29 16:06:49 +0200

    piccoli fix nei dto

--- src/main/java/it/miniatures/paints/dto/PaintResponse.java
+++ src/main/java/it/miniatures/paints/dto/PaintResponse.java
@@ -1,14 +1,23 @@
 package it.miniatures.paints.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
 public class PaintResponse {
+    @Schema(description = "ID univoco", example = "1")
     private Long id;
+    @Schema(description = "Nome del colore", example = "Abaddon Black")
     private String colorName;
+    @Schema(description = "Tipo di vernice", example = "Base")
     private String type;
+    @Schema(description = "Brand della vernice", example = "Citadel")
     private String brand;
+    @Schema(description = "Codice identificativo", example = "21-12")
     private String code;
+    @Schema(description = "Quantità disponibile", example = "3")
     private Integer quantity;
+    @Schema(description = "Data di creazione o aggiornamento", example = "2025-09-29T15:20:00")
     private LocalDateTime data;
 
     public Long getId() { return id; }
